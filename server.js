06a23ade9fe63607d8af2726651c1cf4dc9c8279
ali-linux <mynameisali1999@gmail.com>
2021-05-09 18:11:06 +0300

    final

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
+const path = require("path");
 const port = process.env.PORT || 3000;
 
 // CONNECT TO DATABASE
@@ -12,12 +13,17 @@ app.use(express.json({ extended: false }), (req, res, next) => {
   next();
 });
 
-// app.use()
-app.get("/", (req, res) => res.send("working"));
-
 //Define Routes
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/profile", require("./routes/api/profile"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => console.log("running on port:", port));
